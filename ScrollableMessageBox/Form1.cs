3faca2c2c69168e92d86084b794807e1f6b8cca3
wirlsawyer <wirlsawyer@gmail.com>
2017-03-15 12:36:54 +0800

    First repository

---
+++ ScrollableMessageBox/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ScrollableMessageBox
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var message = "1\n2\n3\n4\n5\n6\n7\n8\n9\n0\n";
+            //assumes it's called from inside another Form
+            ScrollMessageBox.ShowDialog(message, "My Message", this);
+
+           
+
+        }
+    }
+}
